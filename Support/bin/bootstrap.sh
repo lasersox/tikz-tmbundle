e5b6c4ec10e0f1e02db52c3cc574df2103571e7e
Alex <alex@rosiba.com>
2008-10-31 14:05:54 +0100

    Added a Preview TikZ picture command.
    (This requires a patched LaTeX grammar to work.)

---
+++ Support/bin/bootstrap.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+eval tikz=\$$#;
+
+tex="${TMPDIR}/tm-tikz-preview.${TM_PID:-$LOGNAME}.tex"
+pdf="${TMPDIR}/tm-tikz-preview.${TM_PID:-$LOGNAME}.pdf";
+
+pre="${TMPDIR}/tm-tikz-preview-preamble.tex";
+fmt="${TMPDIR}/tm-tikz-preview-preamble.fmt";
+
+if [[ ! -e "$pre" || ! "$(head -n 1 "$pre")" = "    % abd46142bfef7a5f76bc684af041ac48" ]]; then
+  cat > "${pre}" <<-'PREAMBLE'
+    % abd46142bfef7a5f76bc684af041ac48
+    \documentclass{article}
+    \pagestyle{empty}
+    \usepackage{pgfmath}
+    \usepackage{tikz}
+    \usetikzlibrary{%
+      3d,%
+      arrows,%
+      automata,%
+      backgrounds,%
+      calc,%
+      calendar,%
+      chains,%
+      decorations,%
+      decorations.footprints,%
+      decorations.fractals,%
+      decorations.markings,%
+      decorations.pathmorphing,%
+      decorations.pathreplacing,%
+      decorations.shapes,%
+      decorations.text,%
+      er,%
+      fadings,%
+      fit,%
+      folding,%
+      matrix,%
+      mindmap,%
+      patterns,%
+      petri,%
+      plothandlers,%
+      plotmarks,%
+      positioning,%
+      scopes,%
+      shadows,%
+      shapes.arrows,%
+      shapes.callouts,%
+      shapes,%
+      shapes.gates.logic.IEC,%
+      shapes.gates.logic.US,%
+      shapes.geometric,%
+      shapes.misc,%
+      shapes.multipart,%
+      shapes.symbols,%
+      snakes,%
+      through,%
+      topaths,%
+      trees%
+    }
+    \dump
+    \endinput
+PREAMBLE
+  rm -f "${fmt}"
+fi
+
+if [ ! -e "${fmt}" ]; then
+  echo "-->Recompiling fmt"
+  "${@:1:$#-1}" -halt-on-error -output-format=pdf -output-directory="${TMPDIR}" -ini -jobname="tm-tikz-preview-preamble" \&latex "${pre}"
+fi
+
+cat > "${tex}" <<-DOCUMENT
+\\begin{document}
+$(cat "${tikz}")
+\\end{document}
+DOCUMENT
+
+oldtexfmt="$TEXFORMATS"
+export TEXFORMATS=":$TMPDIR:"
+echo "-->Rendering tikzpicture"
+"${@:1:$#-1}" -halt-on-error -output-format=pdf -output-directory="${TMPDIR}" \&tm-tikz-preview-preamble "${tex}"; rc=$?;
+export TEXFORMATS="$oldtexfmt"
+
+if [ ! $rc = 0 ]; then exit $rc; fi
+
